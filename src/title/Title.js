ba0e415f3f4b5c71d0e32d15d133a37583b39c37
Valeriy-Krivosheev <80783944+Valeriy-Krivosheev@users.noreply.github.com>
2022-04-15 13:08:34 +0300

    changed h1

--- src/title/Title.js
+++ src/title/Title.js
@@ -1,8 +1,13 @@
 import './title.css'
 const Title = ({ length}) => {
   return (
+    length === 0?
     <div>
-      <h1 className="wish-title">Общее количество дел: {length} </h1>
+      <h1 className="wish-title">Упс, ты все выполнил :) </h1>
+    </div>
+    :
+    <div>
+        <h1 className="wish-title">Общее количество дел: {length} </h1>
     </div>
   )
 }
